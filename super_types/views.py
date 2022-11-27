66162238658b61b731e92497342d08df246e07eb
Sean Perinuzzi <sperinuzzi@gmail.com>
2022-11-27 16:28:15 -0500

    Made The Hero And Villain Type Through API Requests, Created GET Functions For Both Apps

--- super_types/views.py
+++ super_types/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import View
-from django.shortcuts import get_object_or_404
-from super_types import *
-from supers import *
+from rest_framework import status
+from rest_framework import serializers
+from super_types.serializers import SuperTypeSerializer
+from super_types.models import Super_Types
 # Create your views here.
+
+@api_view(['GET'])
+def Super_types_list(request):
+    super_types=Super_Types.objects.all()
+    serializer = SuperTypeSerializer(super_types, many=True)
+    return Response(serializer.data)
+
+    
