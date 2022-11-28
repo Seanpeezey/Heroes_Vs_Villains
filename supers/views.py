5e49a7c0b6e677fb01dca6df7bb5f901f3778c2e
Sean Perinuzzi <sperinuzzi@gmail.com>
2022-11-27 19:55:14 -0500

    Changed up Code That Would Allow Me To Get Heroes/Villains By Param Function. Tested All API Functions,m And They all Work.

--- supers/views.py
+++ supers/views.py
@@ -11,18 +11,25 @@ from django.shortcuts import get_object_or_404
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
-        supers=Supers.objects.all()
-        serializer = SuperSerializer(supers, many=True)
+        super_type = request.query_params.get('super_type_id')
+        super=Supers.objects.all() 
+        if super_type:
+            super = super.filter(super_types_id=super_type)
+        serializer = SuperSerializer(super, many=True)
         return Response(serializer.data)
+
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
+
+
+
 @api_view(['GET','PUT','DELETE'])
 def supers_details(request, pk):
     super = get_object_or_404(Supers, pk=pk)
+
     if request.method == 'GET':
         serializer = SuperSerializer(super);
         return Response(serializer.data)
